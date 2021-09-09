3ddcbbbb9b990464937e55b4e9d2ffcf9839b1ca
cyberpwn <danielmillst@gmail.com>
2021-09-09 04:10:40 -0400

    Hook into data apis

--- src/main/java/art/arcane/summit/SummitApplication.java
+++ src/main/java/art/arcane/summit/SummitApplication.java
@@ -1,13 +1,22 @@
 package art.arcane.summit;
 
+import art.arcane.summit.security.LudicrousPasswordEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@EntityScan(basePackages = "art.arcane.labyrinth.data.unit")
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SummitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SummitApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new LudicrousPasswordEncoder();
+    }
 }
