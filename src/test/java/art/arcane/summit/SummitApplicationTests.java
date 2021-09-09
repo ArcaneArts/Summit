1140ed200d83117e0223c5c7466da6eecd22a54e
cyberpwn <danielmillst@gmail.com>
2021-09-09 03:42:28 -0400

    Summit server

---
+++ src/test/java/art/arcane/summit/SummitApplicationTests.java
@@ -0,0 +1,13 @@
+package art.arcane.summit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
