42fb0991af02c314233a463c31e60385f7e4613f
cyberpwn <danielmillst@gmail.com>
2021-09-09 04:10:21 -0400

    Sec conf

---
+++ src/main/java/art/arcane/summit/security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package art.arcane.summit.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static art.arcane.summit.security.SummitAuthority.Type.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService uds;
+    private final PasswordEncoder bce;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(uds).passwordEncoder(bce);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests()
+                .antMatchers("api/v1/user/register/**")
+                .permitAll();
+        http.authorizeRequests()
+                .antMatchers("api/**")
+                .authenticated();
+        http.authorizeRequests()
+                .antMatchers("api/v1/admin/**")
+                .hasAnyAuthority(ADMIN.name());
+        http.authorizeRequests().antMatchers("api/v1/devops/**")
+                .hasAnyAuthority(ADMIN.name(), DEVOPS.name());
+        http.addFilter(new art.arcane.summit.security.MasterAuthenticationFilter(authenticationManagerBean()));
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
